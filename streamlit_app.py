51c02f29f07263eb1d951d228b9ae25a6e86480e
ARAVINDANS25 <130492356+ARAVINDANS25@users.noreply.github.com>
2024-06-07 02:22:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,49 +1,42 @@
 import streamlit as st
-import pandas as pd
-from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
+from snowflake.snowpark.functions import col
 
-st.write("""
-# Simple Iris Flower Prediction App
+st.title("🍏🍇🍉 Fruit Selection 🍊🍓🥝")
+st.write(
+    """Welcome to the Fruit Selection! 
+    Choose your favorite fruits and let's create a delicious fruit salad or smoothie! 🥗🍹"""
+)
 
-This app predicts the **Iris flower** type!
-""")
+name_on_order = st.text_input("Name on Smoothie:")
+st.write("The name on your smoothie will be: ", name_on_order)
 
-st.sidebar.header('User Input Parameters')
 
-def user_input_features():
-    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
-    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
-    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
-    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
-    data = {'sepal_length': sepal_length,
-            'sepal_width': sepal_width,
-            'petal_length': petal_length,
-            'petal_width': petal_width}
-    features = pd.DataFrame(data, index=[0])
-    return features
+# Get active Snowflake session
+cnx = st.connection("snowflake")
+session = cnx.session()
 
-df = user_input_features()
+# Execute SQL query to fetch data from Snowflake table
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')) 
 
-st.subheader('User Input parameters')
-st.write(df)
 
-iris = datasets.load_iris()
-X = iris.data
-Y = iris.target
 
-clf = RandomForestClassifier()
-clf.fit(X, Y)
+ingredients_list = st.multiselect(
+    'Choose the ingredients.',
+    my_dataframe,
+    max_selections=5
+)
+if ingredients_list:
+    ingredients_str = ' '.join(ingredients_list)
+    st.write(ingredients_str)
 
-prediction = clf.predict(df)
-prediction_proba = clf.predict_proba(df)
+    # Construct the SQL INSERT statement
+    my_insert_stmt = """
+        INSERT INTO smoothies.public.orders (ingredients, name_on_order)
+        VALUES ('{}', '{}')
+    """.format(ingredients_str, name_on_order)
 
-st.subheader('Class labels and their corresponding index number')
-class_labels = list(enumerate(iris.target_names))
-st.write(class_labels)
+    time_to_insert = st.button("Submit Order")
 
-st.subheader('Prediction')
-st.write(f"Predicted class: {iris.target_names[prediction[0]]} (index: {prediction[0]})")
-
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
